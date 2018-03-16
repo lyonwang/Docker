079fddf632f42a406c0d2e81750f30d1bfa75858
Lyon Wang <lyonwang@boyu66.net>
2018-03-16 18:29:45 +0800

    update

--- rabbitmq/run.bat
+++ rabbitmq/run.bat
@@ -1,4 +1,4 @@
 @echo off
-docker run -d --name rabbitmaster --hostname rabbitmaster -e RABBITMQ_USER=guest -e RABBITMQ_PASS=guest lyonwang/rabbitmq:centos7
+docker run -d --name rabbitmaster --hostname rabbitmaster -e RABBITMQ_USER=guest -e RABBITMQ_PASS=guest -p 15673:15672 -p 5673:5672 registry.boyu66.net/rabbitmq:centos7
 ping 127.0.0.1 -n 11 > nul
-docker run -d --name rabbitslave --hostname rabbitslave -e RABBITMQ_USER=guest -e RABBITMQ_PASS=guest -e CLUSTER_WITH=rabbitmaster -p 15672:15672 -p 5672:5672 --link rabbitmaster lyonwang/rabbitmq:centos7
+docker run -d --name rabbitslave --hostname rabbitslave -e RABBITMQ_USER=guest -e RABBITMQ_PASS=guest -e CLUSTER_WITH=rabbitmaster -p 15672:15672 -p 5672:5672 --link rabbitmaster registry.boyu66.net/rabbitmq:centos7
